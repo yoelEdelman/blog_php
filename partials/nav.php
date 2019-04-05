a5094d998b37fae9fec9342f310717b7ea988d0f
yoelEdelman <yoeledelman@gmail.com>
2019-04-05 16:27:56 +0200

    ajout du boutton profile

--- partials/nav.php
+++ partials/nav.php
@@ -10,6 +10,7 @@
         <!-- ici le boutton de déconnexion est un lien allant vers l'index qui envoie le paramètre "logout" via URL -->
         <p>
             <a class="d-block btn btn-danger mb-4 mt-2" href="index.php?logout">Déconnexion</a>
+            <a class="d-block btn btn-warning mb-4 mt-2" href="user_profile.php">Profile</a>
             <?php if ($_SESSION['user']['is_admin'] == 1): ?>
                 <a class="d-block btn btn-warning mb-4 mt-2" href="admin/index.php">Administration</a>
             <?php endif; ?>
