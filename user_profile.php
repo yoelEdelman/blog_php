4646e809ce5ec86ef371e73e2f5bd539e475dff6
yoelEdelman <yoeledelman@gmail.com>
2019-04-07 01:56:16 +0200

    code fonctionel securite a verifier

--- user_profile.php
+++ user_profile.php
@@ -4,13 +4,6 @@
 $messages = [];
 $warnings = [];
 
-// de base les input sont null = vide
-$first_name = NULL;
-$last_name = NULL;
-$mail = NULL;
-$password = NULL;
-$biography = NULL;
-$is_admin = NULL;
 
 // pour aficher les champs existant pour le update
 if (isset($_GET['user_id'])){
@@ -18,16 +11,9 @@ if (isset($_GET['user_id'])){
     $query_users->execute([
         $_GET['user_id']
     ]);
-    $users = $query_users->fetch();
-
-    $first_name = $users['first_name'];
-    $last_name = $users['last_name'];
-    $mail = $users['mail'];
-    $password = $users['password'];
-    $biography = $users['biography'];
-    $is_admin = $users['is_admin'];
-    $user_id = $users['id'];
+    $user = $query_users->fetch();
 }
+
 // si le formulaire insertion utilisateur/admin a ete envoyer
 if(isset($_POST['update'])) {
     // on verifie que les champs obligatoires sont remplie
@@ -45,27 +31,31 @@ if(isset($_POST['update'])) {
         ];
 
         if (!empty($_POST['password'])) {
-            //concaténation du champ password à mettre à jour
-            $query_string .= ', password = :password ';
-            //ajout du paramètre password à mettre à jour
-            $query_parameters['password'] = md5($_POST['password']);
+            if ($_POST['password'] != $_POST['password_confirm']){
+                $warnings['no_match'] = 'Les mots de passe ne sonts pas identiques !';
+            }
+            else{
+                //concaténation du champ password à mettre à jour
+                $query_string .= ', password = :password ';
+                //ajout du paramètre password à mettre à jour
+                $query_parameters['password'] = md5($_POST['password']);
+            }
         }
 
-        $queryString .= 'WHERE id = :id';
+        $query_string .= ' WHERE id = :id';
 
         $query = $db->prepare($query_string);
         $result = $query->execute($query_parameters);
 
         $messages['updated'] = 'Mise a jour efféctuée avec succes !';
     }
-
-    // on enrengistre tout les input pour pas que l'admin doit tout r'ecrire
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $mail = $_POST['email'];
-    $password = $_POST['password'];
-    $biography = $_POST['bio'];
-    $is_admin = $_POST['is_admin'];
+//    PROBLEME DE PRE REMPLISSAGE
+//    // on enrengistre tout les input pour pas que l'admin doit tout r'ecrire
+//    $first_name = $_POST['first_name'];
+//    $last_name = $_POST['last_name'];
+//    $mail = $_POST['email'];
+//    $password = $_POST['password'];
+//    $biography = $_POST['bio'];
 }
 ?>
 <?php $title = 'Profile - Mon premier blog !'; ?>
@@ -76,19 +66,33 @@ if(isset($_POST['update'])) {
         <div class="row my-3 article-content">
             <?php require('partials/nav.php'); ?>
             <main class="col-9">
-                <form action="user_profile.php" method="post" class="p-4 row flex-column">
+                <!-- on verifie si les 2 tableaux ne sont pas vide pour afficher les massages a l'interieur d'une condition pour gagner en performance-->
+                <?php if (!empty($messages)): ?>
+                    <?php foreach($messages as $key => $message): ?>
+                        <div class="bg-success text-white p-2 mb-4">
+                            <?= $message; ?>
+                        </div>
+                    <?php endforeach; ?>
+                <?php elseif (!empty($warnings)): ?>
+                    <?php foreach($warnings as $key => $warning): ?>
+                        <div class="bg-danger text-white p-2 mb-4">
+                            <?= $warning; ?>
+                        </div>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+                <form action="user_profile.php?user_id=<?= $_SESSION['user']['id']; ?>" method="post" class="p-4 row flex-column">
                     <h4 class="pb-4 col-sm-8 offset-sm-2">Mise à jour des informations utilisateur</h4>
                     <div class="form-group col-sm-8 offset-sm-2">
                         <label for="first_name">Prénom <b class="text-danger">*</b></label>
-                        <input class="form-control" value="User" type="text" placeholder="Prénom" name="first_name" id="first_name" />
+                        <input class="form-control" value="<?= isset($user) ? htmlentities($user['first_name']) : '';?>" type="text" placeholder="Prénom" name="first_name" id="first_name" />
                     </div>
                     <div class="form-group col-sm-8 offset-sm-2">
                         <label for="last_name">Nom de famille</label>
-                        <input class="form-control" value="TheBrickBox" type="text" placeholder="Nom de famille" name="last_name" id="last_name" />
+                        <input class="form-control" value="<?= isset($user) ? htmlentities($user['last_name']) : '';?>" type="text" placeholder="Nom de famille" name="last_name" id="last_name" />
                     </div>
                     <div class="form-group col-sm-8 offset-sm-2">
                         <label for="email">Email <b class="text-danger">*</b></label>
-                        <input class="form-control" value="user@thebrickbox.net" type="email" placeholder="Email" name="email" id="email" />
+                        <input class="form-control" value="<?= isset($user) ? htmlentities($user['mail']) : '';?>" type="email" placeholder="Email" name="email" id="email" />
                     </div>
                     <div class="form-group col-sm-8 offset-sm-2">
                         <label for="password">Mot de passe (uniquement si vous souhaitez modifier votre mot de passe actuel)</label>
@@ -100,12 +104,16 @@ if(isset($_POST['update'])) {
                     </div>
                     <div class="form-group col-sm-8 offset-sm-2">
                         <label for="bio">Biographie</label>
-                        <textarea class="form-control" name="bio" id="bio" placeholder="Ta vie Ton oeuvre...">utilisateur du blog</textarea>
+                        <textarea class="form-control" name="bio" id="bio" placeholder="Ta vie Ton oeuvre..."><?= isset($user) ? htmlentities($user['biography']) : '';?></textarea>
                     </div>
                     <div class="text-right col-sm-8 offset-sm-2">
                         <p class="text-danger">* champs requis</p>
                         <input class="btn btn-success" type="submit" name="update" value="Valider" />
                     </div>
+                    <!-- Si $user existe, on ajoute un champ caché contenant l'id de l'utilisateur à modifier pour la requête UPDATE -->
+                    <?php if(isset($user)): ?>
+                        <input type="hidden" name="user_id" value="<?= $user['id']?>" />
+                    <?php endif; ?>
                 </form>
             </main>
         </div>
