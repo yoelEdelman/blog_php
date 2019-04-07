8fb6f09d4358ddbd91319d3f1e4de1b26f92457d
yoelEdelman <yoeledelman@gmail.com>
2019-04-07 19:34:30 +0200

    user_profile fini (eventuelle securite a voir)

--- user_profile.php
+++ user_profile.php
@@ -1,15 +1,14 @@
-<?php require_once '_tools.php'; ?>
 <?php
+require_once '_tools.php';
 
-$messages = [];
+// On declare un tableau warning pour stocker les eventuellescmessages d'erreur
 $warnings = [];
 
-
 // pour aficher les champs existant pour le update
-if (isset($_GET['user_id'])){
+if (isset($_SESSION['user']['id'])){
     $query_users = $db->prepare('SELECT * FROM user WHERE id = ?');
     $query_users->execute([
-        $_GET['user_id']
+        $_SESSION['user']['id']
     ]);
     $user = $query_users->fetch();
 }
@@ -21,7 +20,9 @@ if(isset($_POST['update'])) {
         $warnings['empty'] = 'Tous les chams sont obligatoire !';
     }
     else{
+        //début de la chaîne de caractères de la requête de mise à jour
         $query_string = 'UPDATE user SET first_name = :first_name, last_name = :last_name, mail = :mail, biography = :biography';
+        //début du tableau de paramètres de la requête de mise à jour
         $query_parameters = [
             'first_name' => htmlspecialchars(ucfirst($_POST['first_name'])),
             'last_name' => htmlspecialchars(strtoupper($_POST['last_name'])),
@@ -30,7 +31,9 @@ if(isset($_POST['update'])) {
             'id' => $_POST['user_id']
         ];
 
+        // si le champs password n'est pas vide
         if (!empty($_POST['password'])) {
+            // on verifie aue les champs password et password_confirm sont identiques
             if ($_POST['password'] != $_POST['password_confirm']){
                 $warnings['no_match'] = 'Les mots de passe ne sonts pas identiques !';
             }
@@ -42,20 +45,28 @@ if(isset($_POST['update'])) {
             }
         }
 
-        $query_string .= ' WHERE id = :id';
+        //on fait le update uniquement dans les cas ou le champ password est vide ou qu'il n'est pas vide et qu'il soit identique au champ password_confirm
+        if (empty($_POST['password']) OR !empty($_POST['password'] AND $_POST['password'] == $_POST['password_confirm'])){
 
-        $query = $db->prepare($query_string);
-        $result = $query->execute($query_parameters);
+            //fin de la chaîne de caractères de la requête de mise à jour
+            $query_string .= ' WHERE id = :id';
 
-        $messages['updated'] = 'Mise a jour efféctuée avec succes !';
+            //préparation et execution de la requête avec la chaîne de caractères et le tableau de données
+            $query = $db->prepare($query_string);
+            $result = $query->execute($query_parameters);
+        }
+        //si enregistrement ok
+        if(isset($result) AND $result){
+            $_SESSION['message']['updated'] = 'Mise a jour efféctuée avec succes !';
+            header('location:index.php');
+            exit;
+        }
     }
-//    PROBLEME DE PRE REMPLISSAGE
-//    // on enrengistre tout les input pour pas que l'admin doit tout r'ecrire
-//    $first_name = $_POST['first_name'];
-//    $last_name = $_POST['last_name'];
-//    $mail = $_POST['email'];
-//    $password = $_POST['password'];
-//    $biography = $_POST['bio'];
+    // on pré remplie tout les input en ecrasant les variables $user pour pas que l'utilisateur doit tout re'ecrire
+    $user['first_name'] = $_POST['first_name'];
+    $user['last_name'] = $_POST['last_name'];
+    $user['mail'] = $_POST['email'];
+    $user['biography'] = $_POST['bio'];
 }
 ?>
 <?php $title = 'Profile - Mon premier blog !'; ?>
@@ -66,29 +77,23 @@ if(isset($_POST['update'])) {
         <div class="row my-3 article-content">
             <?php require('partials/nav.php'); ?>
             <main class="col-9">
-                <!-- on verifie si les 2 tableaux ne sont pas vide pour afficher les massages a l'interieur d'une condition pour gagner en performance-->
-                <?php if (!empty($messages)): ?>
-                    <?php foreach($messages as $key => $message): ?>
-                        <div class="bg-success text-white p-2 mb-4">
-                            <?= $message; ?>
-                        </div>
-                    <?php endforeach; ?>
-                <?php elseif (!empty($warnings)): ?>
+                <!-- on verifie si le tableau $warnings n'est pas vide pour afficher les massages a l'interieur d'une condition pour gagner en performance-->
+                <?php if (!empty($warnings)): ?>
                     <?php foreach($warnings as $key => $warning): ?>
                         <div class="bg-danger text-white p-2 mb-4">
                             <?= $warning; ?>
                         </div>
                     <?php endforeach; ?>
                 <?php endif; ?>
-                <form action="user_profile.php?user_id=<?= $_SESSION['user']['id']; ?>" method="post" class="p-4 row flex-column">
+                <form action="user_profile.php" method="post" class="p-4 row flex-column">
                     <h4 class="pb-4 col-sm-8 offset-sm-2">Mise à jour des informations utilisateur</h4>
                     <div class="form-group col-sm-8 offset-sm-2">
                         <label for="first_name">Prénom <b class="text-danger">*</b></label>
-                        <input class="form-control" value="<?= isset($user) ? htmlentities($user['first_name']) : '';?>" type="text" placeholder="Prénom" name="first_name" id="first_name" />
+                        <input class="form-control" value="<?= isset($user) ? htmlentities($user['first_name']) : '' ;?>" type="text" placeholder="Prénom" name="first_name" id="first_name" />
                     </div>
                     <div class="form-group col-sm-8 offset-sm-2">
                         <label for="last_name">Nom de famille</label>
-                        <input class="form-control" value="<?= isset($user) ? htmlentities($user['last_name']) : '';?>" type="text" placeholder="Nom de famille" name="last_name" id="last_name" />
+                        <input class="form-control" value="<?= isset($user) ? htmlentities($user['last_name']) : '' ;?>" type="text" placeholder="Nom de famille" name="last_name" id="last_name" />
                     </div>
                     <div class="form-group col-sm-8 offset-sm-2">
                         <label for="email">Email <b class="text-danger">*</b></label>
