aa95a02e31107bc787c86fc5da1ab122d6c1948d
yoelEdelman <yoeledelman@gmail.com>
2019-04-11 23:34:23 +0200

    if admin hide profile button

--- partials/nav.php
+++ partials/nav.php
@@ -10,10 +10,13 @@
         <!-- ici le boutton de déconnexion est un lien allant vers l'index qui envoie le paramètre "logout" via URL -->
         <p>
             <a class="d-block btn btn-danger mb-4 mt-2" href="index.php?logout">Déconnexion</a>
-            <!-- pour ne pas afficher le bouton profile quand on est sur la page user_profile !-->
-            <?php if ($_SERVER['SCRIPT_FILENAME'] != $_SERVER['DOCUMENT_ROOT'].='/user_profile.php' ): ?>
-            <a class="d-block btn btn-warning mb-4 mt-2" href="user_profile.php">Profile</a>
+            <?php if ($_SESSION['user']['is_admin'] != 1): ?>
+                <!-- pour ne pas afficher le bouton profile quand on est sur la page user_profile !-->
+                <?php if ($_SERVER['SCRIPT_FILENAME'] != $_SERVER['DOCUMENT_ROOT'].='/user_profile.php' ): ?>
+                    <a class="d-block btn btn-warning mb-4 mt-2" href="user_profile.php">Profile</a>
+                <?php endif; ?>
             <?php endif; ?>
+
             <?php if ($_SESSION['user']['is_admin'] == 1): ?>
                 <a class="d-block btn btn-warning mb-4 mt-2" href="admin/index.php">Administration</a>
             <?php endif; ?>
